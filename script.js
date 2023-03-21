55196bc9a17cfc809a93b919a25077d37a086c3e
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-03-21 14:19:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+async function fetchData(callback){
+	const response = await fetch(url);
+	const data = await response.json();
+	callback(data);
+}
+function msg(data){
+	document.getElementById("output").innerText = data.title;
+}
